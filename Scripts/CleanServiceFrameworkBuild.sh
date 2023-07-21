46a47518167e81db9ad8ed6881146dff583485db
The-Arkitekt <mr.davidcrumley@gmail.com>
2023-07-21 12:40:36 -0400

    Added clean build script

---
+++ Scripts/CleanServiceFrameworkBuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BLD_PATH=$PWD/../$1/bld
+
+echo "Cleaning ServiceFramework build for: ${1}"
+
+if [ -d ${BLD_PATH} ]
+then 
+  cd ${BLD_PATH}
+  echo "Entered build directory: ${PWD}"
+  
+  rm -rf *
+  echo "Build directory contents after clean: ${ls}"
+else
+  echo "Invalid build directory: ${BLD_PATH}"
+fi
+
+exit
